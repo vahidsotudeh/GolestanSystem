df9e51a2c295f67f334463b4c8715b5f93b6b58b
Ali Asghar <taghizade12@gmail.com>
2017-05-28 19:50:44 +0430

    nothing special

--- src/main/java/ir/sbu/golestan/repository/SubGroupRepository.java
+++ src/main/java/ir/sbu/golestan/repository/SubGroupRepository.java
@@ -1,10 +1,12 @@
 package ir.sbu.golestan.repository;
 
 import ir.sbu.golestan.domain.SubGroup;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by Ali Asghar on 22/05/2017.
  */
-public interface SubGroupRepository extends CrudRepository<SubGroup,Long > {
+@RepositoryRestResource(path = "sub_groups")
+public interface SubGroupRepository extends PagingAndSortingRepository<SubGroup,Long > {
 }

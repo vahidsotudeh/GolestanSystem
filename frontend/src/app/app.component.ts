b2e2bfff6e34201e94925423895639d9c76b92a8
vahidsotudeh@gmail.com <vahidsotudeh@gmail.com>
2017-06-19 00:16:19 +0430

    angular layouts-component added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation} from '@angular/core';
+import { CoursesComponent } from './courses/courses.component';
+import {routing} from './app.routes';
+import {Router} from '@angular/router';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../assets/css/master.css'
+  ]
 })
 export class AppComponent {
+    public router : Router;
+    flagTest = true;
+    constructor(r: Router) {
+        this.router =r;
+    }
+
+  public courseManagement(){
+    this.router.navigate(['/courses']);
+    this.flagTest=false;
+
+  }
   title = 'app';
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {routing} from './app.routes';
+// import {HttpModule, Headers, Response, ResponseOptions} from "@angular/http";
+// import {RestangularConfigFactory} from './services/config'
+import { RestangularModule,Restangular  } from 'ngx-restangular';
+import { RouterModule, Routes } from '@angular/router';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+
+export function RestangularConfigFactory (RestangularProvider) {
+  RestangularProvider.setBaseUrl('http://217.182.230.17:8080');
+  //RestangularProvider.setFullResponse(true);
+  RestangularProvider.addResponseInterceptor((data, operation, what, url, response)=> {
+       console.log(data.length);
+       console.log(url);
+       console.log(what);
+
+       return data;
+    });
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RestangularModule.forRoot(RestangularConfigFactory),
+    routing,
+    Ng2Bs3ModalModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
